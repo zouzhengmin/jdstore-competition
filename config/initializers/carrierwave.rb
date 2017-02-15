6d7a94001590dc74b53dc00cba0e7f2daf56d5aa
罗心 <xiaoxindihua@hotmail.com>
2017-02-15 14:24:02 +0800

    install fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,14 +6,14 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV["AKIAJLLTBAY5OTMJO4KA"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["AKIAJLLTBAY5OTMJO4KA"],      # 你的 secret key
+      aws_secret_access_key: ENV["rfkks0CXDE0cm7U8DFDgHmwXfZbarAHmp5lSiFwO"],      # 你的 secret key
 
 
       region:                'Oregon' # 你的 S3 bucket 的 Region 位置
 
 
     }
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
+    config.fog_directory  = ENV["jdstore-lx"] # 你设定的 bucket name
 
 
 
