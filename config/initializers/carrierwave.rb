4d476754986aab667b4a35933a729b6784eb9b26
罗心 <xiaoxindihua@hotmail.com>
2017-02-15 18:52:46 +0800

    yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,36 @@
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_credentials = {
+#       provider:              'AWS',
+#       aws_access_key_id:     ENV["aws_access_key_id"],      # 你的 key
+#
+#
+#       aws_secret_access_key: ENV["aws_secret_access_key"],      # 你的 secret key
+#
+#
+#       region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
+#
+#
+#     }
+#     config.storage :fog
+#     config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
+#
+#
+#
+#   else
+#     config.storage :file
+#   end
+# end
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["aws_access_key_id"],      # 你的 key
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:               'AWS',                        # required
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],     # required
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'], # required
+    region:                 ENV['AWS_REGION'],            # optional, defaults to 'us-east-1'
 
-
-      aws_secret_access_key: ENV["aws_secret_access_key"],      # 你的 secret key
-
-
-      region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
-
-
-    }
-    config.storage :fog
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
-
-
-
-  else
-    config.storage :file
-  end
+  }
+  config.fog_directory  = 'discoveredfmyelpdemo'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
