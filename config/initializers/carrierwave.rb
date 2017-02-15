ba930163dd04d154ed7b4ea2f57f34692ad8da76
罗心 <xiaoxindihua@hotmail.com>
2017-02-15 14:34:06 +0800

    install fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAJLLTBAY5OTMJO4KA"],      # 你的 key
+      aws_access_key_id:     ENV["AKIAI4GTSMHJUJZL7JQQ"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["rfkks0CXDE0cm7U8DFDgHmwXfZbarAHmp5lSiFwO"],      # 你的 secret key
+      aws_secret_access_key: ENV["5TE5cFuogR0JmnPhRasR/jdAa1FB23JKEYjkOBh0"],      # 你的 secret key
 
 
       region:                'Oregon' # 你的 S3 bucket 的 Region 位置
