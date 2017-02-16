8b2f5dd931795302bc347c46ae6e924dea36a185
罗心 <xiaoxindihua@hotmail.com>
2017-02-17 00:38:04 +0800

    add key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,10 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
+      aws_access_key_id:     ENV["AKIAJS2FV2ZLJADAHDJQ"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
+      aws_secret_access_key: ENV["/wp+XQ81x2BFiuROj++kYYslUBHA+t+ysBi53B65"],      # 你的 secret key
 
 
       region:                'ap-northeast-2' # 你的 S3 bucket 的 Region 位置
